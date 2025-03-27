3e281952c9b3881a80d2ed3820d79d49cfdfc95e
Connor Pletikapich <contorresplet@gmail.com>
2025-03-26 22:39:15 -0400

    Added Order.cs and OrderItem.cs (PP.29-30)

--- flying.nomad.Data/StoreContext.cs
+++ flying.nomad.Data/StoreContext.cs
@@ -6,7 +6,8 @@ namespace flying.nomad.Data {
     public class StoreContext : DbContext {
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
         public DbSet<Item> Items { get; set; }
-
+        
+        public DbSet<Order> Orders { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
 {
     base.OnModelCreating(builder);
